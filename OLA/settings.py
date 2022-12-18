bd9b0fec5ca18530898909a410b424f3aa548cfe
mad-coder <0030bscs2017@gmail.com>
2022-12-18 18:57:18 +0500

    ss

--- OLA/settings.py
+++ OLA/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOWED_ORIGINS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_METHODS = [
     "DELETE",
     "GET",
